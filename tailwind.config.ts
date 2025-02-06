c61e5eca52f4598e485dac7fcdc1af8ac44b584a
Michael Zhao <michaelzhao314@gmail.com>
2025-02-06 01:32:49 -0600

    Replaced defaults with background and loaded fonts/colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                purple: '#A06FD6',
+                pink: '#EA5FD0',
+                blue: '#2157A9',
+                purpleLight: '#B791D0'
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
